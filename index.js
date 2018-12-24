7211015ab3d6c5079c6038c08ae1fedb46fa0d64
RGHuynh <randall.g.huynh@gmail.com>
2018-12-24 18:06:38 -0500

    Change PORT variable to include process.env.PORT

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 4000;
 
 app.use(cors({origin: true}));
 
@@ -9,7 +8,8 @@ app.get('/', (req, resp, next) => {
     resp.send('Hello World');
 });
 
-console.log('you are now in port 4000');
-
-app.listen(PORT);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Mixing it up on port ${PORT}`);
+});
 
