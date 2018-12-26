121d9c7a9be798f2edbd13c65bc55b9084d431dc
RGHuynh <randall.g.huynh@gmail.com>
2018-12-26 13:15:38 -0500

    Fix overflow-x for mobile

--- client/src/components/topPick/index.jsx
+++ client/src/components/topPick/index.jsx
@@ -11,14 +11,28 @@ export default function TopPick() {
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                     </p>
                 </div>
-                <div className="article-container">
-                    <div className="article-image-wrapper">
+
+                <div className="articles-container">
+                    <div className="article-container">
+                        <div className="article-image-wrapper">
+                        </div>
+                        <div className="article-descript-wrapper">
+                            <p className="article-descript">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                        <div className="article-arthur-wrapper">
+                            <h6 className="article-arthur">authorname</h6>
+                        </div>
                     </div>
-                    <div className="article-descript-wrapper">
-                        <p className="article-descript">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                    <div className="article-arthur-wrapper">
-                        <h6 className="article-arthur">authorname</h6>
+
+                    <div className="article-container">
+                        <div className="article-image-wrapper">
+                        </div>
+                        <div className="article-descript-wrapper">
+                            <p className="article-descript">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                        <div className="article-arthur-wrapper">
+                            <h6 className="article-arthur">authorname</h6>
+                        </div>
                     </div>
                 </div>
             </div>
