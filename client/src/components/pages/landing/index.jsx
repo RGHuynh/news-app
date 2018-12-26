494488e74ab5bcbab8ad95e923461aacb48551e2
RGHuynh <randall.g.huynh@gmail.com>
2018-12-25 23:35:36 -0500

    Setup frontend

---
+++ client/src/components/pages/landing/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+import Navigation from '../../navigations'
+
+export default function Landing() {
+    return(
+        <main>
+            <Navigation />
+        </main>
+    )
+}
